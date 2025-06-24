a30539f6eedf52c17950d432665c063edc654f20
EduardoM <eduardomeza540@gmail.com>
2025-06-23 20:38:51 -0700

    project factory

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import { createTodo } from "./todo";
+import { createProject } from "./project";
 
 const newTask = createTodo('Hello', 'greet some1', '2025-06-29', 'medium')
 
 console.log(newTask);
+
+const myProject = createProject('MyProject');
+
+console.log(myProject);
